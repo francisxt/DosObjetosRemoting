99dbc571ef46631b0ba96e8dde527d0b1344aa5f
francisxt <francisxt@live.com>
2018-07-10 02:31:48 -0500

    Add files via upload

---
+++ DosObjetosRemoting/Componente/ComponenteA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Componente
+{
+    /********************************
+     *  Aplicaciones Distribuidas   *
+     *  Francisco Gualli            *
+     *  Gr1                         *
+     *  Practica 05                 *
+     *  Componente                  *
+     /*******************************/
+    public class ComponenteA : MarshalByRefObject
+    {
+        public ComponenteA()
+        {
+            Log.Imprimir("Se creo una instancia del Objeto Remoto ComponenteA");
+        }
+
+        public string Llamada()
+        {
+            Log.Imprimir("Se invoco a Llamada()");
+            return "ComponenteA.Llamada()";
+        }
+    }
+    public class ComponenteB : MarshalByRefObject
+    {
+        public ComponenteB()
+        {
+            Log.Imprimir("Se creo una instancia del Objeto Remoto ComponenteB");
+        }
+        public string Llamada()
+        {
+            Log.Imprimir("Se invoco a Llamada()");
+            return "ComponenteB.Llamada()";
+        }
+
+    }
+}
---
+++ DosObjetosRemoting/Servidor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using Componente;
+
+
+    /********************************
+     *  Aplicaciones Distribuidas   *
+     *  Francisco Gualli            *
+     *  Gr1                         *
+     *  Practica 05                 *
+     *  Servidor                    *
+     /*******************************/
+
+//Lance otro Cliente (puede ir a la carpeta Debug, buscar el ejecutable y presionar doble
+//clic). Cuál es el resultado?
+
+//Se puede ejecutar multiples clientes simúltaneamente.
+
+namespace Servidor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.Configure("Servidor.exe.config");
+            Utilidades.MostrarTodosLosDatos();
+            Log.EsperarParaTerminar("Presione ENTER para detener al servidor...");
+        }
+    }
+}
